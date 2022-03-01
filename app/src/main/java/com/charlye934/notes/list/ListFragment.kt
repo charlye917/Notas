4956e3bd0a7a5ac8a00cfc1522642cf066ba92f6
Charlye Arteaga <charlyearteaga@MacBook-Pro-de-Charlye.local>
2022-02-28 21:54:41 -0600

    se creo primeras vistas

---
+++ app/src/main/java/com/charlye934/notes/list/ListFragment.kt
@@ -0,0 +1,42 @@
+package com.charlye934.notes.list
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.charlye934.notes.R
+import com.charlye934.notes.databinding.FragmentListBinding
+
+class ListFragment : Fragment() {
+
+    private lateinit var binding: FragmentListBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentListBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setOnclickListener()
+
+        setHasOptionsMenu(true)
+    }
+
+    private fun setOnclickListener(){
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        binding.listLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
+
+}
